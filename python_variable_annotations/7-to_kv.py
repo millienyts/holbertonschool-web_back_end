7530efad24bd121eccf0bffe0462cd62db2d497a
millienytorressanta <tmillieny@gmail.com>
2024-04-23 13:37:46 -0400

    annotations

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,8 +6,9 @@
                  should be annotated as a float.
     Arguments: k: str, v: Union[int, float]
 '''
-from typing import Tuple, Union
+from typing import Union, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Returns a tuple with string k and the square of int/float v."""
+    """Returns a tuple with the string k and the square of int/float v."""
     return (k, v ** 2)
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+#!/usr/bin/env python3
+''' Description: takes a float multiplier as argument and returns a function
+                 that multiplies a float by multiplier
+    Arguments: multiplier: float
+'''
+
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Returns a function that multiplies a float by the multiplier."""
+    """Returns a function that multiplies a float by multiplier."""
     def multiplier_function(x: float) -> float:
         return x * multiplier
     return multiplier_function
