df3fd0873d4fa4bb5fb08189ee88b23952d6f9f9
millienytorressanta <tmillieny@gmail.com>
2024-04-23 13:21:43 -0400

    annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-    Type-annotated function add that takes a float 'a' and a float 'b' as
+    Type-annotated function add that takes a float a and a float b as
     arguments and returns their sum as a float.
 """
 
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+    Type-annotated function add that concats 'str1' and
+    float 'str2' as a single str
+"""
 
 def concat(str1: str, str2: str) -> str:
     """Concatenates two strings and returns the result."""
