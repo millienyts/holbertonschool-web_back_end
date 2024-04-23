7de2266e0ca468f56b063b2f2a692c99a0d6c540
millienytorressanta <tmillieny@gmail.com>
2024-04-23 13:07:28 -0400

    annotations

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
-"""
-    Type-annotated function add that concats 'str1' and
-    float str2 as a single str
-"""
 
-
-def add(str1: str, str2: str) -> str:
-    """
-        Returns a str concat of str1 + str2
-    """
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the result."""
     return str1 + str2
