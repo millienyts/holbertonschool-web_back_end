6bfd916e4e5243b3d197d8f59656e097f845b078
millienytorressanta <tmillieny@gmail.com>
2024-04-23 13:28:11 -0400

    annotations

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+''' This function graciously accepts a list of floating-point numbers as its input and then, like a diligent accountant, meticulously adds them all up, returning the grand total as a shiny float. It's like a skilled conductor harmonizing a symphony of numbers into a beautiful sum.
+'''
 from typing import List
 
 def sum_list(input_list: List[float]) -> float:
