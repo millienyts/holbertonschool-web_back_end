c096600045702bdf16d890ea27c61f9247e67b9a
millienytorressanta <tmillieny@gmail.com>
2024-04-23 13:30:51 -0400

    annotations

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 from typing import List, Union
+''' Description: takes a list mxd_lst of floats and integers and
+    returns their sum as a float.
+    Arguments: mxd_lst: List[Union[int, float]]
+'''
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns the sum of a list of integers and floats."""
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+''' Description: takes a string k and an int OR float v as arguments and
+                 returns a tuple. The first element of the tuple is the
+                 string k.
+                 The second element is the square of the int/float v and
+                 should be annotated as a float.
+    Arguments: k: str, v: Union[int, float]
+'''
 from typing import Tuple, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
