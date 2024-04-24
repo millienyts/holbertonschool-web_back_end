303b965d992d45d997753ff0adaa10600105488b
millienytorressanta <tmillieny@gmail.com>
2024-04-24 17:55:16 -0400

    annotations

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-''' This function graciously accepts a list of floating-point numbers as its input and then, like a diligent accountant, meticulously adds them all up, returning the grand total as a shiny float. It's like a skilled conductor harmonizing a symphony of numbers into a beautiful sum.
+''' Description: akes a list input_list of floats as argument and
+    returns their sum as a float.
+    Arguments: input_list: List[float]
 '''
+
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats."""
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-from typing import List, Union
 ''' Description: takes a list mxd_lst of floats and integers and
     returns their sum as a float.
     Arguments: mxd_lst: List[Union[int, float]]
 '''
 
+from typing import List, Union
+
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """Returns the sum of a list of integers and floats."""
+    """Returns the sum of a mixed list of integers and floats."""
     return sum(mxd_lst)
