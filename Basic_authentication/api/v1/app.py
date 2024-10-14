24166345e2cc5cb50ad5021741f68d15b8299054
Millienyts <tmillieny@gmail.com>
2024-10-13 23:33:50 -0400

    authention

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,3 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/status', methods=['GET'])
+def status():
+    return {"status": "OK"}, 200
+
 if __name__ == "__main__":
-    print("App is running")
+    host = "0.0.0.0"
+    port = 5000
+    app.run(host=host, port=port, debug=True)
 
